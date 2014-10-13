4331a0135fbf3c3de70450606d7b721127c42b59
gzpgg3x <gzpgg3x@yahoo.com>
2014-10-12 21:07:18 -0400

    appearsPresentable
    needFurtherValidate

--- fromScratch/hackEdukate/hackEdukate/settings.py
+++ fromScratch/hackEdukate/hackEdukate/settings.py
@@ -12,6 +12,22 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+from unipath import Path
+PROJECT_DIR = Path(__file__).parent
+
+from decouple import config
+import dj_database_url
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG', default=False, cast=bool)
+TEMPLATE_DEBUG = DEBUG
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -30,12 +46,22 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'django.contrib.admin',    
+    # 'south',
+    'hackEdukate.activities',
+    'hackEdukate.articles',
+    'hackEdukate.auth',
+    'hackEdukate.core',
+    'hackEdukate.feeds',
+    'hackEdukate.messages',
+    'hackEdukate.questions',
+    'hackEdukate.search',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -43,6 +69,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -62,12 +89,16 @@ DATABASES = {
     }
 }
 
+ROOT_URLCONF = 'hackEdukate.urls'
+
+WSGI_APPLICATION = 'hackEdukate.wsgi.application'
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -80,3 +111,30 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOCALE_PATHS = (PROJECT_DIR.child('locale'), )
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_ROOT = PROJECT_DIR.parent.child('staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    PROJECT_DIR.child('static'),
+)
+
+MEDIA_ROOT = PROJECT_DIR.parent.child('media')
+MEDIA_URL = '/media/'
+
+TEMPLATE_DIRS = (
+    PROJECT_DIR.child('templates'),
+)
+
+LOGIN_URL = '/'
+LOGIN_REDIRECT_URL = '/feeds/'
+
+ALLOWED_SIGNUP_DOMAINS = ['*']
+
+FILE_UPLOAD_TEMP_DIR = '/tmp/'
+FILE_UPLOAD_PERMISSIONS = 0644
